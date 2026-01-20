f07b831cae004a44009e580669f6e2f1e790b12c
Дмитрий <45686133+j2m5@users.noreply.github.com>
2026-01-20 12:42:49 +0300

    fix audio metadata

--- src/ui/mobX/AudioPlayerStore.ts
+++ src/ui/mobX/AudioPlayerStore.ts
@@ -1,7 +1,7 @@
 import StarMap from '@/ui/components/modules/audio/tracks/StarMap.ogg'
 import { ITrack } from '@/ui/components/modules/audio/interfaces/ITrack'
 import { makeAutoObservable, runInAction } from 'mobx'
-import { IAudioMetadata, parseBlob } from 'music-metadata-browser'
+import { IAudioMetadata, parseBlob } from 'music-metadata'
 import { notificationStore } from '@/ui/mobX/NotificationStore'
 
 const defaultTracks: ITrack[] = [
